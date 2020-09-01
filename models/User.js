0bde90a5dd49d7b4461063e46d0ce6dba3e91549
agimenezgor <alvarogimenez88@gmail.com>
2020-09-01 18:33:12 +0200

    modificados campos del usuario

--- models/User.js
+++ models/User.js
@@ -14,7 +14,30 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: [true, 'La contraseña es necesaria'],
         minlength: [8, 'La contraseña debe tener como mínimo 8 caracteres']
-    }
+    },
+    role: {
+        type: String,
+        default: 'usuario',
+        enum: ['admin', 'vendedor', 'usuario']
+    },
+    // Administrador
+        equipo: Array,
+
+    // Vendedor
+        // Ventas realizadas por el vendedor
+        actosVenta: {
+            type: Array,
+        },
+
+    // Usuario
+        // dirección del usuario
+        direccion: {
+            type: String,
+        },
+        // Compras realizadas por el usuario
+        actosCompra: {
+            type: Array,
+        },
 });
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
