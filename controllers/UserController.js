c4d9b05877b5048bd8fdfa4142cc0ce8a4fd612b
agimenezgor <alvarogimenez88@gmail.com>
2020-09-02 18:35:15 +0200

    buscar por email y login implementados

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -10,11 +10,19 @@ const UserController = {
              res.status(500).send({message:'There was a problem trying to get the users', error});
          }
     },
+    async getByEmail(req, res){
+        try {
+            const user = await User.findOne({email:req.params.email});
+            res.send(user);
+         } catch (error) {
+             console.log(error);
+             res.status(500).send({message:'There was a problem trying to get the user', error});
+         }
+    },
     // Registra un usuario en la base de datos
     async register(req, res){
         // falta determinar un rol de usuario
         try {
-            console.log(req.body);
             const user = await User.create(req.body);
             res.send({user, message: 'Usuario creado correctamente'});
         } catch (error) {
@@ -35,6 +43,21 @@ const UserController = {
         } catch (error) {
             console.error(error);
             res.status(500).send({message:'There was a problem trying to delete the user', error});
+        }
+    },
+    async login(req, res){
+        try {
+            const user = await User.findOne({email:req.params.user});
+            if(!user){
+                res.send({message: 'El usuario no existe en la base de datos'});
+            }else if(user.password != req.params.password){
+                res.send({message: 'La contraseña es incorrecta'});
+            }else{res.send(
+                {message: 'Sesión iniciada correctamente', user})
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({message:'There was a problem trying to login the user', error});
         }
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,16 @@ const UserController = require('../controllers/UserController');
 // retorna todos los usuarios
 router.get('/', UserController.getAll);
 
+// retorna un usuario a traves de su email
+router.get('/email/:email', UserController.getByEmail);
+
 // inserta un usuario en la base de datos
 router.post('/', UserController.register);
 
 // borra un usuario en la base de datos
 router.delete('/:email', UserController.delete);
+
+// Logea un usuario a traves de su email y su contraseña
+router.get('/login/:user/:password', UserController.login);
 
 module.exports = router;
